4b9364679c3b850a9cba873d4c9a67ccdb52e596
itsabdulahad <55660777+itsabdulahad@users.noreply.github.com>
2019-09-22 21:10:10 +0530

    Add files via upload

---
+++ problem3.py
@@ -0,0 +1,11 @@
+lst = []
+n = int(input("enter number of elements: "))
+for i in range (0,n):
+	ele = int(input())
+	lst.append(ele)
+print(lst)
+
+search_element=int(input("enter element to be searched"))
+
+for i in range(0,n):
+	print(i)
